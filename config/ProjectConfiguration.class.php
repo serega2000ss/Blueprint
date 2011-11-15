23cedb98f4dd98887665c8c2bcf5601bcce2fe0c
Sergey Korolyuk <serega2000ss@gmail.com>
2011-11-15 15:45:46 +0200

    adding arcticles and categories

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -12,5 +12,6 @@ class ProjectConfiguration extends sfProjectConfiguration
     $this->enablePlugins('sfJqueryReloadedPlugin');
     $this->enablePlugins('sfAdminDashPlugin');
     $this->enablePlugins('sfCaptchaGDPlugin');
+    $this->enablePlugins('sfDoctrineActAsTaggablePlugin');
   }
 }

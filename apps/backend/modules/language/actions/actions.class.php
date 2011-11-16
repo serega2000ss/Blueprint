93b82d1ba5ad9549f5d1a69a7cf27742f816e95f
Sergey Korolyuk <serega2000ss@gmail.com>
2011-11-16 11:09:05 +0200

    adding languages

---
+++ apps/backend/modules/language/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/languageGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/languageGeneratorHelper.class.php';
+
+/**
+ * language actions.
+ *
+ * @package    blueprint
+ * @subpackage language
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class languageActions extends autoLanguageActions
+{
+}
--- lib/form/doctrine/LanguageForm.class.php
+++ lib/form/doctrine/LanguageForm.class.php
@@ -1,17 +1,16 @@
 <?php
 
 /**
- * Category form.
+ * Language form.
  *
  * @package    blueprint
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class CategoryForm extends BaseCategoryForm
+class LanguageForm extends BaseLanguageForm
 {
-    public function configure()
-    {
-        $this->embedI18n(LanguageTable::getLanguagesArray());
-    }
+  public function configure()
+  {
+  }
 }

ef0de0a43e7e40757415caeeb285adce8b3b436b
Sergey Korolyuk <serega2000ss@gmail.com>
2011-11-16 10:23:15 +0200

    adding articles and categories link

--- lib/form/doctrine/CategoryForm.class.php
+++ lib/form/doctrine/CategoryForm.class.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * ArticleCategoryTranslation form.
+ * Category form.
  *
  * @package    blueprint
  * @subpackage form
  * @author     Your name here
  * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class ArticleCategoryTranslationForm extends BaseArticleCategoryTranslationForm
+class CategoryForm extends BaseCategoryForm
 {
   public function configure()
   {
